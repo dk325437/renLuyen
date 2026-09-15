8811da3dc19a6ebb162ace08908ee28d6b3f8462
gg459974 <pndkhoa459974@gmail.com>
2026-09-15 15:03:39 +0700

    init

--- app/src/main/java/com/example/test6/MainActivity.java
+++ app/src/main/java/com/example/test6/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.test6;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    Button btLoad;
+    List countryList = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        countryList.add("VietNam");
+        countryList.add("US");
+        countryList.add("China");
+        countryList.add("Japan");
+        countryList.add("Korea");
+        countryList.add("ThaiLand");
+
+        recyclerView = findViewById(R.id.recyclerView);
+        btLoad = findViewById(R.id.btLoad);
+      btLoad.setOnClickListener(v -> {
+              MyAdapter myAdapter = new MyAdapter(v.getContext(),countryList);
+              recyclerView.setLayoutManager(new LinearLayoutManager(this));
+              recyclerView.setAdapter(myAdapter);
+      }
+      );
+
+
     }
 }
